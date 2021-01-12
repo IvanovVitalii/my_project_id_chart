296cfbfe36586c23cfb1d8acdbbd7356cd5639b4
Ivanov Vitalii <ivanov.v.v13@gmail.com>
2021-01-12 11:01:38 +0200

    start collecting html

--- mysite/mysite/apps/id_chart/views.py
+++ mysite/mysite/apps/id_chart/views.py
@@ -1,41 +1,35 @@
-import os
+from django.http import Http404
+from django.shortcuts import render
 from django.http import HttpResponse
-from datetime import datetime
-
 from .maths_id_chart import *
 
 
-def index(request):
+def index(request, flow=None, process=None, t1=None, fi1=None, d1=None, h1=None):
 
     plt.subplots(figsize=(6, 10))
     plt.axis([0, 30, -25, 55])
     plot_id()
 
     z0 = cooling(t1=32, fi1=40, flow=1000, t2=20)
-    z1 = heat(t1=z0['t2'], fi1=z0['fi2'], flow=z0['flow'], t2=40)
-    z2 = humidification(t1=z1['t2'], fi1=z1['fi2'], flow=z1['flow'], fi2=40)
 
     data = save_bytes()
 
-    return HttpResponse(f'process 1:{z0["process"]}, '
-                        f't1={z0["t1"]}, '
-                        f't2={z0["t2"]}, '
-                        f'flow={z0["flow"]}, '
-                        f'power: {z0["power"]} kW '
-                        '<br>'
-                        f'process 2:{z1["process"]}, '
-                        f't1={z1["t1"]}, '
-                        f't2={z1["t2"]}, '
-                        f'flow={z1["flow"]}, '
-                        f'power: {z1["power"]} kW '
-                        '<br>'
-                        f'process 3:{z2["process"]}, '
-                        f'fi1={z2["fi1"]}, '
-                        f'fi2={z2["fi2"]}, '
-                        f'd1={z2["d1"]}, '
-                        f'd2={z2["d2"]}, '
-                        f'flow={z2["flow"]}, '
-                        f'power: {z2["power"]} kW, '
-                        f'humidity inflow: {z2["humidity_inflow"]} kg '
-                        '<br>'
-                        f"<img src='data:image/png;base64,{data}'/>")
+    id_image = f'data:image/png;base64,{data}'
+
+    context = {
+        'flow': z0['flow'],
+        't1': z0['t1'],
+        'fi1': z0['fi1'],
+        'd1': z0['d1'],
+        'h1': z0['h1'],
+        'process': z0['process'],
+        't2': z0['t2'],
+        'fi2': z0['fi2'],
+        'd2': z0['d2'],
+        'h2': z0['h2'],
+        'power': z0['power'],
+        'humidity_inflow': z0['humidity_inflow'],
+        'id_image': id_image
+    }
+
+    return render(request, 'id_chart/id_chart.html', context=context)
